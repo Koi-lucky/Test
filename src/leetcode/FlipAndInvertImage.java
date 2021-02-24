f190a40cc5b7b287e076aa4ce70dd3c783ef706f
Koi-lucky <2465585664@qq.com>
2021-02-24 22:05:11 +0800

    Test提交代码

---
+++ src/leetcode/FlipAndInvertImage.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * TODO
+ *
+ * @author Chen
+ * @date 2021/2/24 22:04
+ */
+public class FlipAndInvertImage {
+
+    public int[][] flipAndInvertImage(int[][] A) {
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                if (A[i][left] == A[i][right]) {
+                    A[i][left] ^= 1;
+                    A[i][right] ^= 1;
+                }
+                left++;
+                right--;
+            }
+            if (left == right) {
+                A[i][left] ^= 1;
+            }
+        }
+        return A;
+    }
+}
