6f565720adf12c2f6d2cff4156bee8dff05f27ae
Koi-lucky <2465585664@qq.com>
2021-02-25 23:38:57 +0800

    Test提交代码

---
+++ src/leetcode/Transpose.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * TODO
+ *
+ * @author Chen
+ * @date 2021/2/25 23:38
+ */
+public class Transpose {
+
+    public int[][] transpose(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[][] transposed = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+}
