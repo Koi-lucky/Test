316c9189d3b65181db99d0448592e57299cf736f
Koi-lucky <2465585664@qq.com>
2021-02-28 23:14:03 +0800

    Test提交代码

---
+++ src/leetcode/IsMonotonic.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import org.junit.Test;
+
+/**
+ * TODO
+ *
+ * @author Chen
+ * @date 2021/2/28 23:12
+ */
+public class IsMonotonic {
+
+    int[] A = {6,5,4,4};
+
+    @Test
+    public void isMonotonic() {
+        boolean inc = true, dec = true;
+        int n = A.length;
+        for (int i = 0; i < n - 1; ++i) {
+            if (A[i] > A[i + 1]) {
+                inc = false;
+            }
+            if (A[i] < A[i + 1]) {
+                dec = false;
+            }
+        }
+        System.out.println(inc || dec);
+    }
+}
