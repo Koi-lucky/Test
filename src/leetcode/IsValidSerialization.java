c3b70da4cd27428b52dfc7c695e8687ad9585525
Koi-lucky <2465585664@qq.com>
2021-03-12 22:25:48 +0800

    Test提交代码

---
+++ src/leetcode/IsValidSerialization.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * TODO
+ *
+ * @author Chen
+ * @date 2021/3/12 22:25
+ */
+public class IsValidSerialization {
+
+    public boolean isValidSerialization(String preorder) {
+        int n = preorder.length();
+        int i = 0;
+        Deque<Integer> stack = new LinkedList<Integer>();
+        stack.push(1);
+        while (i < n) {
+            if (stack.isEmpty()) {
+                return false;
+            }
+            if (preorder.charAt(i) == ',') {
+                i++;
+            } else if (preorder.charAt(i) == '#'){
+                int top = stack.pop() - 1;
+                if (top > 0) {
+                    stack.push(top);
+                }
+                i++;
+            } else {
+                // 读一个数字
+                while (i < n && preorder.charAt(i) != ',') {
+                    i++;
+                }
+                int top = stack.pop() - 1;
+                if (top > 0) {
+                    stack.push(top);
+                }
+                stack.push(2);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
