5bd18c848ce0f5c99216f9daf522ad539269ae05
Koi-lucky <2465585664@qq.com>
2021-03-25 22:24:43 +0800

    Test提交代码

---
+++ src/leetcode/DeleteDuplicates.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+/**
+ * TODO
+ *
+ * @author Chen
+ * @date 2021/3/25 22:24
+ */
+public class DeleteDuplicates {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0, head);
+
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
+    }
+
+
+}
