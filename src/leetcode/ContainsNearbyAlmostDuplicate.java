4830387b55d6bc2459a90ed8d8d6b313cea01be3
Koi-lucky <2465585664@qq.com>
2021-04-17 23:40:59 +0800

    Test提交代码

---
+++ src/leetcode/ContainsNearbyAlmostDuplicate.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.TreeSet;
+
+/**
+ * TODO
+ *
+ * @author Chen
+ * @date 2021/4/17 23:40
+ */
+public class ContainsNearbyAlmostDuplicate {
+
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        int n = nums.length;
+        TreeSet<Long> set = new TreeSet<Long>();
+        for (int i = 0; i < n; i++) {
+            Long ceiling = set.ceiling((long) nums[i] - (long) t);
+            if (ceiling != null && ceiling <= (long) nums[i] + (long) t) {
+                return true;
+            }
+            set.add((long) nums[i]);
+            if (i >= k) {
+                set.remove((long) nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
